7d4c270a93eb397ec021faf6c22a733d63fe7307
HajarElMannani <laso125@hotmail.com>
2024-01-08 11:58:49 -0800

    Fix errors

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,5 +1,5 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef PAL_H
+#define PAL_H
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -21,4 +21,4 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int is_palindrome(listint_t **head);
 
-#endif /* LISTS_H */
+#endif /* PAL_H */
