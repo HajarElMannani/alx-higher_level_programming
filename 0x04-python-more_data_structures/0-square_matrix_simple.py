b39e2260586f81776fc7f585c61d6f82a91b1e39
HajarElMannani <laso125@hotmail.com>
2024-01-10 06:33:03 -0800

    First commit

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new = []
+    for i in matrix:
+        m = []
+        new.append(m)
+        for l in i:
+            m.append(l ** 2)
+    return new
