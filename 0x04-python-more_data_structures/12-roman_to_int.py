657d0c1d4fc250194cf5e3380bcdec69b8a6f757
HajarElMannani <laso125@hotmail.com>
2024-01-10 12:08:05 -0800

    First commit

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    numbers = {"i": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M":100}
+    if len(roman_string) == 1:
+        return numbers.get(roman_string)
+    if type(roman_string) is not str  or roman_string is None:
+        return 0
+    sum = 0
+    for i in range(len(roman_string)):
+        if len(roman_string) == i + 1:
+            sum = sum numbers.get(roman_string[i])
+        elif numbers.get(roman_string[i]) < numbers.get(roman_string[i + 1]):
+            sum = sum - numbers.get(roman_string[i])
+        else:
+            sum = sum + numbers.get(roman_string[i])
+    return sum
