3493c72ecced1a5807ae3f7f9a1d843e7945692e
HajarElMannani <laso125@hotmail.com>
2024-01-10 06:49:24 -0800

    First commit

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    nlist = []
+    for elem in mylist:
+        if i != search:
+            nlist.append(elem)
+        else:
+            nlist.append(replace)
+    return nlist
