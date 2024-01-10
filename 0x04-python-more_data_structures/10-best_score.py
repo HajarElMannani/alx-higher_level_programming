a11602d1f7e1d298f446a2afb46df1491df81718
HajarElMannani <laso125@hotmail.com>
2024-01-10 11:16:25 -0800

    First commit

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    else:
+        score = 0
+        for x, y in a_dictionary.items():
+            if y > value:
+                value = j
+                name = i
+        return name
