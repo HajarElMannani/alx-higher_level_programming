0ccb367a5c712e51a3b8a13363990fdf91d8de2b
HajarElMannani <laso125@hotmail.com>
2024-01-10 06:54:03 -0800

    Fix error

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     nlist = []
-    for elem in mylist:
-        if i != search:
+    for elem in my_list:
+        if elem != search:
             nlist.append(elem)
         else:
             nlist.append(replace)
