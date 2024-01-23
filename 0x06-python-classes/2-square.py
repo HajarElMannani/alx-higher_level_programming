d41cb2c7449ba24819813b31cea86441f1231e37
HajarElMannani <laso125@hotmail.com>
2024-01-23 05:33:06 -0800

    First commit

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""class Square that defines a square by size"""
+
+
+class Square:
+    """defines a square by size"""
+    def __init__(self, size=0):
+        """Instantiation with size
+        args:
+            size (int): size of the square
+        Returns:
+            nothing
+        """
+        if type(size) is int:
+            self.__size = size
+            if size < 0:
+                raise ValueError('size must be >= 0')
+        else:
+            raise TypeError('size must be an integer')
