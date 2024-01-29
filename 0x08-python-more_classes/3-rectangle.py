2fc305b44d5677612dda22a5952e36cb48428911
HajarElMannani <laso125@hotmail.com>
2024-01-29 08:07:25 -0800

    change condition

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -64,7 +64,7 @@ class Rectangle:
     def __str__(self):
         '''print rectangle'''
         ch = ""
-        if self.__width != 0 or self.__height != 0:
+        if self.__width != 0 and self.__height != 0:
             for i in range(self.__height):
                 for j in range(self.__width):
                     ch += "#"
