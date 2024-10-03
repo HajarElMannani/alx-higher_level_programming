967b082c644270ba75bc2fd363912966f037858e
HajarElMannani <laso125@hotmail.com>
2024-10-03 00:52:37 -0700

    add condition

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    copy_string = list(my_string)
-    i = 0
-    for a in copy_string:
-        if a == 'c' or a == 'C':
-            copy_string[i] = ""
-            i = i + 1
-    return "".join(copy_string)
+    ''' a function that removes c and C from a string'''
+    new_string = ""
+    for char in my_string:
+        if (ord(char) == 67) or (ord(char) == 99):
+            continue
+        new_string = new_string + char
+    return new_string
