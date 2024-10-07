0947febdfcd4c09ea5f93ca6c5ced90d856671c9
HajarElMannani <laso125@hotmail.com>
2024-10-07 14:35:49 -0700

    change module

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -99,5 +99,4 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         '''returns a square'''
-        new_rectangle = Rectangle(size, size)
-        return new_rectangle
+        return cls(size, size)
