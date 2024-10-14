89024958758ea463073a758b918e73462cee3a08
HajarElMannani <laso125@hotmail.com>
2024-10-14 13:30:54 -0700

    First commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''empty class BaseGeometry'''
+
+
+class BaseGeometry:
+    '''empty class'''
+
+    def area(self):
+        '''raise an exception
+         Return: nothing
+        '''
+        raise Exception("area() is not implemented")
