092ccd42c5dfff0039ee8f3a3d2f7a918a2173a7
HajarElMannani <laso125@hotmail.com>
2024-10-14 11:41:28 -0700

    First commit

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''class that inherits from list'''
+
+
+class MyList(list):
+    '''a class that enherits from the class list'''
+
+    def print_sorted(self):
+        '''print a sorted list'''
+        print("{}".format(sorted(self)))
