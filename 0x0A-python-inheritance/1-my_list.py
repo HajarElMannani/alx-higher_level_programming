acb2facc0499d606e7e2d94a1f7e874ab68b8c86
HajarElMannani <laso125@hotmail.com>
2024-10-14 13:05:29 -0700

    pycode style

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,5 +6,10 @@ class MyList(list):
     '''a class that enherits from the class list'''
 
     def print_sorted(self):
-        '''print a sorted list'''
+        '''print a sorted list
+        args:
+            self(any): object
+        Return:
+            Nothing
+        '''
         print(sorted(self))
