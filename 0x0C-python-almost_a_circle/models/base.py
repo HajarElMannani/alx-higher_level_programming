498b71d50bb51087bc0f609094c8287a9e66e960
HajarElMannani <laso125@hotmail.com>
2024-10-21 13:24:37 -0700

    remove condition

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -65,9 +65,9 @@ class Base():
         Return:  instance with all attributes already set'''
         if dictionary and dictionary != {}:
             if cls.__name__ == "Rectangle":
-                dummy = cls(1, 1, 1, 1)
-            if cls.__name__ == "Square":
                 dummy = cls(1, 1, 1)
+            else:
+                dummy = cls(1, 1)
             dummy.update(**dictionary)
             return dummy
 
