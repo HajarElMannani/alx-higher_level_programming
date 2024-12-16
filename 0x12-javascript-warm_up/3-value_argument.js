33f6fc3739c67cc1f00361980aecec928a5ade66
HajarElMannani <laso125@hotmail.com>
2024-12-16 11:06:03 -0800

    modify statement

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 let count = 0;
-let i;
 argv.forEach((val, index) => {
   count += 1;
 });
 if (count === 2) {
   console.log('No argument');
 } else {
-  for (i = 2; i < count; i++) {
-    console.log(argv[i]);
-  }
+  console.log(argv[2]);
 }
