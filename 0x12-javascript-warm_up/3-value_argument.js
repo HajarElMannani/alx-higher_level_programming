3162f0a2838c6438fb965a435f44fd07a2d5a7ee
HajarElMannani <laso125@hotmail.com>
2024-12-16 10:57:58 -0800

    semistandard

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 let count = 0;
-
+let i;
 argv.forEach((val, index) => {
   count += 1;
 });
 if (count === 2) {
   console.log('No argument');
 } else {
-  for (let i = 2; i < count; i++) {
+  for (i = 2; i < count; i++) {
     console.log(argv[i]);
   }
 }
