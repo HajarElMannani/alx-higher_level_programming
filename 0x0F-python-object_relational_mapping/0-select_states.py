4d03f4af82c532e3afd2fb747faa10338ca80541
HajarElMannani <laso125@hotmail.com>
2024-12-24 15:14:21 -0800

    add hostname

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,5 +6,10 @@ from sys import argv
 if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    [print(state) for state in cur.fetchall()]
+    cur.execute('SELECT * FROM states ORDER BY ASC')
+    rows = cur.fetchall()
+    for j in rows:
+        print(j)
+
+    cur.close()
+    db.close()
