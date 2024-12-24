05203976737333a1892fc84b0888e4ad63d8c593
HajarElMannani <laso125@hotmail.com>
2024-12-24 15:27:47 -0800

    change structure

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,15 +3,13 @@
 import MySQLdb
 from sys import argv
 
-
+if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    cur.execute('SELECT * FROM states ORDER BY ASC')
-    rows = cur.fetchall()
-    for j in rows:
-        print(j)
+    c = db.cursor()
+    c.execute('SELECT * FROM states ORDER BY ASC')
+    for state in c.fetchall():
+        print(state)
 
-    cur.close()
+    c.close()
     db.close()
-if __name__ == '__main__':
-    main()
+
