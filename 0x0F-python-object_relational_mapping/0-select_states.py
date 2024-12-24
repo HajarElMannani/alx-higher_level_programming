7b46a4c6af2da070b14bb4a4ca177b07e7903dde
HajarElMannani <laso125@hotmail.com>
2024-12-24 15:17:34 -0800

    add hostname

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,7 +3,7 @@
 import MySQLdb
 from sys import argv
 
-if __name__ == '__main__':
+
     db = MySQLdb.connect(host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute('SELECT * FROM states ORDER BY ASC')
@@ -13,3 +13,5 @@ if __name__ == '__main__':
 
     cur.close()
     db.close()
+if __name__ == '__main__':
+    main()
