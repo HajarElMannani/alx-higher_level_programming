784f2af91af955fda52e2a8d484150804098f4b3
HajarElMannani <laso125@hotmail.com>
2024-12-24 15:10:22 -0800

    add hostname

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 #script that lists all states from the database hbtn_0e_0_usa
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(host='hostname', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
     [print(state) for state in cur.fetchall()]
