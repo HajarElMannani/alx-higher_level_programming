8b865ff6bc91c65c1be03b4fa76fabf92a39469b
HajarElMannani <laso125@hotmail.com>
2024-12-27 15:32:50 -0800

    First commit

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -18,8 +18,8 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    searched = session.query(State).filter_by(State.name == argv[4])
+    searched = session.query(State).filter(State.name == argv[4]).first()
     if (searched is None):
         print("Not found")
     else:
-        print("{}".format(inst.id))
+        print("{}".format(searched.id))
