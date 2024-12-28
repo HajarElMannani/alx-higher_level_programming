c4eb5ed6d19fa0b5d267d791b92a60c5dff3e2cb
HajarElMannani <laso125@hotmail.com>
2024-12-28 03:32:07 -0800

    First commit

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''
-Script that prints the State object with the name passed
- as argument from the database hbtn_0e_6_usa
+script that deletes all State objects with a name containing
+ the letter a from the database hbtn_0e_6_usa
 '''
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
