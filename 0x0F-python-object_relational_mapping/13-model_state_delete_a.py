c38581e0e2c6a9a321bb8c93ab33c5fca832aa31
HajarElMannani <laso125@hotmail.com>
2024-12-28 03:52:32 -0800

    First commit

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -18,6 +18,6 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    searched = session.query(State).filter(State.name.ilike='%a%')
-    for i in searched:
-        session.delete(i)
+    for inst in session.query(State).filter(State.name.ilike('%a%')):
+        session.delete(inst)
+    session.commit()
