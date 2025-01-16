7e674193c11faa73de37b9000390dd8230b99704
HajarElMannani <laso125@hotmail.com>
2025-01-16 14:11:10 -0800

    First commit

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 #Script that takes in a URL, sends a GET request to
 #the URL, and displays the body of the response
 curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
