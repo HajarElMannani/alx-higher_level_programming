16cb2e9cded4bfeb53a39324ed818d11911795f2
HajarElMannani <laso125@hotmail.com>
2025-01-16 14:41:35 -0800

    First commit

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -s -I -X OPTIONS "$1" | grep -i "allow" | cut -d " " -f2-
+curl -sI -X OPTIONS $1 | grep -i Allow: | cut -d " " -f2-
