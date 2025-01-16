76e5416fd1a34105a75fea1bf968183e13e6779d
HajarElMannani <laso125@hotmail.com>
2025-01-16 13:52:04 -0800

    First commit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # script that takes in a URL, sends a request to
 #that URL, and displays the size of the body of the response
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 URL"
-  exit 1
-fi
-
-curl -s -o /dev/null -w '%{size_download}\n' "$1"
+curl -sI '$1' | grep -i 'Content-Length' | cut -d " " -f2
