f7c15291abaab407b5228a22f818f66d92737666
HajarElMannani <laso125@hotmail.com>
2025-01-17 14:13:27 -0800

    First commit

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Script that takes in a URL and an email, sends a POST request to the
+ passed URL with the email as a parameter, and displays the body of the
+ response'''
+import urllib.request
+import urllib.parse
+from sys import argv
+
+
+if __name__ == "__main__":
+    data = urllib.parse.urlencode({'email': argv[2]})
+    data = argv[2].encode('ascii')
+    req = urllib.request.Request(argv[1], data)
+    with urllib.request.urlopen(req) as resp:
+        body = resp.read()
+    print(body)
