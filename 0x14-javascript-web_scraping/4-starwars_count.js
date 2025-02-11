947ea10c0996688446acc3164fa7fed245095184
HajarElMannani <laso125@hotmail.com>
2025-02-11 13:46:58 -0800

    Add return

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,8 @@ const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/people/18/';
 request(url, function (error, response, body) {
   if (error) {
-    console.error(error);
+      console.error(error);
+      return;
   }
   console.log(JSON.parse(body).films.length);
 });
