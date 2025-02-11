1d4d36fb35da0322327b9a757e18b6743b312a66
HajarElMannani <laso125@hotmail.com>
2025-02-11 15:55:20 -0800

    change conditions

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,21 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-//let fullUrl = url + '?userid';
-//let dict = {}
 request(url, function (error, response, body) {
-    if (error) {
-	console.error(error);
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const tasks = JSON.parse(body);
+  const dict = {};
+  tasks.forEach((task) => {
+    if (task.completed) {
+      if (dict[task.userId] === undefined) {
+        dict[task.userId] = 1;
+      } else {
+        dict[task.userId] += 1;
+      }
     }
-    const tasks = JSON.parse(body);
-let dict = {}
-    tasks.forEach((task) => {
-	if (dict[task.userId] === undefined) {
-		dict[task.userId] = 1;
-	} else {
-	    dict[task.userId] += 1;
-	}
-    });
-    console.log(dict);
-
+  });
+  console.log(dict);
 });
