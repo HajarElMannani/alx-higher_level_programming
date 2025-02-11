13de46ac612d5905525f8b4bf91af7e206f8219f
HajarElMannani <laso125@hotmail.com>
2025-02-11 14:27:04 -0800

    Change code

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,22 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/people/18/';
+const url = process.argv[2];
 request(url, function (error, response, body) {
   if (error) {
-      console.error(error);
-      return;
+    console.error(error);
+    return;
+  }
+  const results = JSON.parse(body).results;
+  if (!results) {
+    console.error(error);
+  }
+});
+const req = require('request');
+const url2 = 'https://swapi-api.alx-tools.com/api/people/18/';
+req(url2, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    return;
   }
   console.log(JSON.parse(body).films.length);
 });
